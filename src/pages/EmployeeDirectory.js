ad0d79fc6657be45e2ad5323d0b468a0ef534a2f
kaylamuraoka <kmurs98@gmail.com>
2021-02-26 15:21:18 -1000

    adding pages forder with simple about and employee directory page

---
+++ src/pages/EmployeeDirectory.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Table from "../components/Table";
+
+function EmployeeDirectory() {
+  return (
+    <div>
+      <h1>Employee Directory</h1>
+      <Table />
+    </div>
+  );
+}
+export default EmployeeDirectory;
