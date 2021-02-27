f851860ba0e051bf13beb7bdd422a9de1d374c5f
kaylamuraoka <kmurs98@gmail.com>
2021-02-27 02:48:00 -1000

    updating Banner component with styling

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./style.css";
+import { Link } from "react-router-dom";
 
 function Banner(props) {
   return (
     <div
-      className="banner text-center"
-      style={{ backgroundImage: `url(${props.backgroundImage})` }}
+      className="banner-image"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${props.backgroundImage})`,
+      }}
     >
-      {props.children}
+      <div className="banner-text">
+        <h1>{props.title}</h1>
+        <p>{props.subtitle}</p>
+        <br />
+
+        <Link to={props.buttonRoute}>
+          <button>{props.buttonText} </button>
+        </Link>
+      </div>
     </div>
   );
 }
