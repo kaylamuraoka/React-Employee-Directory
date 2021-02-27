8599a84a69d0ea0736570165cfda483d4984d382
kaylamuraoka <kmurs98@gmail.com>
2021-02-26 18:09:07 -1000

    modified the EmployeeDirectory page to use newly added components

--- src/pages/EmployeeDirectory.js
+++ src/pages/EmployeeDirectory.js
@@ -1,13 +1,27 @@
 import React from "react";
-
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
 import Table from "../components/Table";
 
 function EmployeeDirectory() {
   return (
-    <div>
-      <h1>Employee Directory</h1>
-      <Table />
-    </div>
+    <Container>
+      <h1 className="text-center">Employee Directory</h1>
+      <hr />
+      <p className="text-center">
+        This employee directory is populated with randomly generated users via
+        an API call to the Random User API. This API simply generates random
+        user data (shown below). You may search for specific employees via the
+        search field, sort a field (Name, Email, DOB) in ascending or descending
+        order.
+      </p>
+      <Row>
+        <Col size="col-12">
+          <Table />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 export default EmployeeDirectory;
