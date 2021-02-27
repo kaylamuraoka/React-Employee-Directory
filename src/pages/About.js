12377157b70a268e2306ce3dd95baf5303297b6d
kaylamuraoka <kmurs98@gmail.com>
2021-02-27 02:48:50 -1000

    updating About page to unclude necessary prop values

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,24 +1,18 @@
 import React from "react";
 import Banner from "../components/Banner";
-import Container from "../components/Container";
 
 function About() {
   return (
     <div>
-      <Banner backgroundImage="https://content.linkedin.com/content/dam/business/marketing-solutions/global/en_US/blog/2017/06/WorkplaceHeader.jpg">
-        <h1>About This App</h1>
-        <hr />
-        <Container>
-          <p style={{ color: "white" }}>
-            This React app gives business professionals the ability to access
-            non-sensitive data about their employees, and easily search, filter,
-            and sort through their entire directory.
-          </p>
-        </Container>
-        <button className="btn mt-3">
-          <a href="/employee-directory">View Employee Directory</a>
-        </button>
-      </Banner>
+      <Banner
+        backgroundImage="https://content.linkedin.com/content/dam/business/marketing-solutions/global/en_US/blog/2017/06/WorkplaceHeader.jpg"
+        title="About This App"
+        subtitle="This React app gives business professionals the ability to access
+        non-sensitive data about their employees, and easily search, filter,
+        and sort through their entire directory."
+        buttonText="View Employee Directory"
+        buttonRoute="/employee-directory"
+      ></Banner>
     </div>
   );
 }
