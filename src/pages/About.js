728f52ab89206531e3b3e150f35a86c36f1d857d
kaylamuraoka <kmurs98@gmail.com>
2021-02-26 18:09:32 -1000

    modified About page to use newly added components

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Banner from "../components/Banner";
+import Container from "../components/Container";
 
 function About() {
   return (
     <div>
-      <h1>Welcome to the About page</h1>
+      <Banner backgroundImage="https://content.linkedin.com/content/dam/business/marketing-solutions/global/en_US/blog/2017/06/WorkplaceHeader.jpg">
+        <h1>About This App</h1>
+        <hr />
+        <Container>
+          <p style={{ color: "white" }}>
+            This React app gives business professionals the ability to access
+            non-sensitive data about their employees, and easily search, filter,
+            and sort through their entire directory.
+          </p>
+        </Container>
+        <button className="btn mt-3">
+          <a href="/employee-directory">View Employee Directory</a>
+        </button>
+      </Banner>
     </div>
   );
 }
