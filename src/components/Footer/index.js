14ba279a1f0c04e429b01060ff708d08d2f2b875
kaylamuraoka <kmurs98@gmail.com>
2021-02-26 15:23:04 -1000

    adding Footer component that will be imported into App.js

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer p-2">
+      <span>© Copyright 2020 Kayla Muraoka</span>
+    </footer>
+  );
+}
+
+export default Footer;
