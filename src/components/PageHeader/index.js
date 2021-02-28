0503f349979ba82a034c8fa1b0381d8ec5c79819
kaylamuraoka <kmurs98@gmail.com>
2021-02-27 23:51:32 -1000

    updating page header component

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./style.css";
+import { FiSearch } from "react-icons/fi";
+import { IconContext } from "react-icons";
 
 function PageHeader(props) {
   return (
@@ -13,6 +15,25 @@ function PageHeader(props) {
         <h1>{props.title}</h1>
         <p>{props.subtitle}</p>
         <br />
+
+        <div className="input-icons">
+          <IconContext.Provider
+            value={{
+              color: "grey",
+              className: "icon",
+            }}
+          >
+            <i>
+              <FiSearch />
+            </i>
+          </IconContext.Provider>
+          <input
+            className="input-field"
+            type="text"
+            onkeyup={props.onkeyupFunction}
+            placeholder="Search for employee.."
+          ></input>
+        </div>
       </div>
     </div>
   );
